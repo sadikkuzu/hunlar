0b715679b36e7d61bbdb9963bd6e96cd082545c3
SADIK KUZU <sadikkuzu@hotmail.com>
2020-05-18 19:07:55 +0300

    kimler-oynuyor-log

--- kimler-oynuyor-cron.sh
+++ kimler-oynuyor-cron.sh
@@ -6,4 +6,5 @@ cmp --silent ~/hunlar/kimler-oynuyor-cron.tmp ~/hunlar/kimler-oynuyor-simdi.tmp 
  cat ~/hunlar/kimler-oynuyor-simdi.tmp >> ~/hunlar/kimler-oynuyor-simdi.txt
  bash ~/hunlar/kimler-oynuyor-simdi.sh >> ~/hunlar/kimler-oynuyor-simdi.txt
  for i in {1..3}; do echo " " >> ~/hunlar/kimler-oynuyor-simdi.txt; done
+ cat ~/hunlar/kimler-oynuyor-simdi.txt >> ~/hunlar/kimler-oynuyor-log.txt
 }
