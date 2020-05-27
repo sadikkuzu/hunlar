5f1d86c36f18034ddbaa6341ddd19d2244f36838
SADIK KUZU <sadikkuzu@hotmail.com>
2020-05-28 02:11:52 +0300

    try except __name__ __main__ etc

--- telegram_send.py
+++ telegram_send.py
@@ -1,20 +1,27 @@
-#! /usr/bin/python
+#! /usr/bin/python3
 # SADIK KUZU (c) 2020
 
-import telepot
-from telegram_credentials import hun, csgo
-from araclar import simdi
+try:
+    import telepot
+    from telegram_credentials import hun, csgo
+    from araclar import simdi
+except:
+    import sys
+    sys.exit('There aint any required package(s).')
 
-bot = telepot.Bot(hun)
+def hunlar():
+    bot = telepot.Bot(hun)
+    f = open(simdi, 'r')
+    data = f.readlines()
+    tire = '---------------------------------------------------------------------------\n'
+    tire2 = '===========================================================================\n'
+    while tire in data:
+        data.remove(tire)
+    while tire2 in data:
+        data.remove(tire2)
+    dat = ''.join(data)
+    msj = dat.strip()
+    resp = bot.sendMessage(csgo, msj)
 
-f = open(simdi, 'r')
-data = f.readlines()
-tire = '---------------------------------------------------------------------------\n'
-tire2 = '===========================================================================\n'
-while tire in data:
-    data.remove(tire)
-while tire2 in data:
-    data.remove(tire2)
-dat = ''.join(data)
-msj = dat.strip()
-resp = bot.sendMessage(csgo, msj)
+if __name__ == "__main__":
+    hunlar()
